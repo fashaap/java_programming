04b60e4b824c0f0d7ebabfdf3edfee8e1661c6d3
Phoenix <140335324+fashaap@users.noreply.github.com>
2025-10-28 15:45:19 +0700

    Add files via upload

---
+++ praktikum/TP_MODUL_02/deklarasi_variable.java
@@ -0,0 +1,11 @@
+public class main{
+	public static void main(String[] args){
+		int Umur = 20;
+		double Tinggi = 175.5;
+		String Nama = "Andi";
+		
+		System.out.println("Umur: " + Umur);
+		System.out.println("Tinggi: " + Tinggi);
+		System.out.println("Nama: " + Nama);
+	}
+}
