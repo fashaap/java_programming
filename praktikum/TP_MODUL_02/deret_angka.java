91d063a395e2bbc5c1192774b7f20d317ea0e5d2
Phoenix <140335324+fashaap@users.noreply.github.com>
2025-10-29 10:15:53 +0700

    Add files via upload

---
+++ praktikum/TP_MODUL_02/deret_angka.java
@@ -0,0 +1,8 @@
+public class main{
+	public static void main(String[] args){
+        double penjumlahan = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+        System.out.println("Hasil penjumlahan deret adalah: " + penjumlahan);
+	}	
+}
+
+		
