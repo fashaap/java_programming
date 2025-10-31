eed5cf9251b3ffb85028daa7b3a055ccfd4d4056
fashaap <bmpasha23@gmail.com>
2025-10-31 15:07:30 +0700

    file baru

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main{
+	public static void main(String[] args){	
+		System.out.println("Welcome to Java");
+		System.out.println("im telkom university student");	
+		System.out.println("i think, Programming is fun");		
+	}
+}
