f8e6f69a93973b4ae28b7a38dc4548184d02ca9b
Phoenix <140335324+fashaap@users.noreply.github.com>
2025-11-01 17:57:15 +0700

    Add files via upload

---
+++ praktikum/TP_MODUL_03/tugas_02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Masukan nilai boolean : ");
+		boolean pencetak_nilai = input.nextBoolean();
+		
+		System.out.println(pencetak_nilai);
+		
+	
+	}
+}
