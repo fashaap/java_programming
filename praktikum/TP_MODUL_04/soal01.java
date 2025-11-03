85c0b1920e0e7398c69e9726f1539bc2c222d489
Phoenix <140335324+fashaap@users.noreply.github.com>
2025-11-03 07:03:11 +0700

    Add files via upload

---
+++ praktikum/TP_MODUL_04/soal01.java
@@ -0,0 +1,14 @@
+public class tugas01{
+	public static void main(String[] args){
+		int intNum = 5;
+		int intOther = 10;
+		float sngNum = -3;
+		
+
+
+		if(!(intOther >= intNum)){
+			System.out.println("!(intOther >= intNum"); //true
+		}
+
+	}
+}
