54276b075db46462be50a46c2aa80dc0cd5401f9
Phoenix <140335324+fashaap@users.noreply.github.com>
2025-11-20 15:22:33 +0700

    Add files via upload

---
+++ praktikum/TP_MODUL_07/soal01.java
@@ -0,0 +1,11 @@
+public class soal01{
+	public static void main(String[] args){
+		
+		for(int i = 1; i <= 9; i++){
+			int perkalian = 3 * i;
+			
+			System.out.println("3 * " + i + " = " + perkalian);
+		}
+		
+	}
+}
